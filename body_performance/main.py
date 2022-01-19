e40aa417134ed55cf0debc47a6bf559c04130efd
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2022-01-19 20:53:20 +0900

    [기타]: 여러 라이브러리 테스트

--- body_performance/main.py
+++ body_performance/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import pandas as pd
 
 x = np.arange(3)
 print(x)
@@ -10,3 +12,13 @@ plt.bar(x, values)
 plt.xticks(x, years)
 
 plt.show()
+
+f = open('bodyPerformance.csv', 'r', encoding='utf-8')
+rdr = csv.reader(f)
+
+count = 0
+for line in rdr:
+  print(line)
+  count += 1
+  if count == 10:
+    break

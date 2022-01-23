6eebfce9bc607a1e3eb24e2bee386296a217a948
nebulayoon4700@gmail.com <nebulayoon4700@gmail.com>
2022-01-23 21:54:35 +0900

    [기능추가]: age에 따른 class dictionary sorted

--- body_performance/main.py
+++ body_performance/main.py
@@ -18,6 +18,8 @@ import pandas as pd
 f = open('bodyPerformance.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
 
+
+
 count = 0
 for line in rdr:
   print(line)
@@ -25,8 +27,27 @@ for line in rdr:
   if count == 10:
     break
 
-bp = pd.read_csv("bodyPerformance.csv")
+bp = pd.read_csv("bodyPerformance.csv", header="infer")
 
-selectdata = pd.DataFrame(bp, columns=["gender", "systolic"])
-
+selectdata = pd.DataFrame(bp, columns=["age", "class"])
+print("select data console")
 print(selectdata)
+
+# for column_name, item in selectdata.iteritems():
+#   print(column_name)
+#   print(item[0])
+
+min_age = min(selectdata.loc[:, 'age'])
+max_age = max(selectdata.loc[:, 'age'])
+
+dic = {}
+for i in range(len(selectdata)):
+  selected_age = int(selectdata.loc[i, 'age'])
+
+  if selectdata.loc[i, 'class'] == 'A' and selected_age not in dic.keys():
+    dic[selected_age] = 1
+  elif selectdata.loc[i, 'class'] == 'A' and selected_age in dic.keys():
+    dic[selected_age] += 1
+
+dic = sorted(dic.items())
+print(dic)
